495674f228ac715ba35328739ce4c7fd64fa234b
Victor Oliveira <victor.souza.41@hotmail.com>
2021-12-15 21:11:13 -0300

    O que foi alterado/adicionado:
    1-) Pastas
     Criado as pastas "Context", "EntitiesConfiguration", "Identity", e "Repositories".
    
    2-) ApplicationDbContext.cs
     Criado classe para estabelecer conexão com o BD.
    
    3-) Bibliotecas
     Realizado a instalação dos pacotes: Microsoft.EntityFrameworkcore.SqlServer, Microsoft.EntityFrameworkcore.Tools e Microsoft.EntityFrameworkcore.Design.

---
+++ CleanArchMVC.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchMVC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
