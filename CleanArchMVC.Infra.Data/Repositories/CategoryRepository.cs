8dce2ef0e7869564ac4793a9d4e74c7d6c2b22c0
Victor Oliveira <victor.souza.41@hotmail.com>
2021-12-20 18:41:09 -0300

    subindo alterações

---
+++ CleanArchMVC.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+    }
+}
