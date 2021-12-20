c15d99ca702b6bb77a8a961cac0cc059feea9518
Victor Oliveira <victor.souza.41@hotmail.com>
2021-12-20 18:57:22 -0300

    O que foi alterado/adicionado:
    1-) CategoryRepository.cs
     Implementado classe concreta e seus respectivos métodos, que herda de ICategoryRepository.

--- CleanArchMVC.Infra.Data/Repositories/CategoryRepository.cs
+++ CleanArchMVC.Infra.Data/Repositories/CategoryRepository.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using CleanArchMVC.Domain.Entities;
+using CleanArchMVC.Domain.Interface;
+using CleanArchMVC.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CleanArchMVC.Infra.Data.Repositories
 {
     public class CategoryRepository : ICategoryRepository
     {
+        ApplicationDbContext _categoryContext;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _categoryContext = context;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            _categoryContext.Add(category);
+            await _categoryContext.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<Category> GetByIdAsync(int? id)
+        {
+            return await _categoryContext.Categories.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await _categoryContext.Categories.ToListAsync();
+        }
+
+        public async Task<Category> RemoveAsync(Category category)
+        {
+            _categoryContext.Remove(category);
+            await _categoryContext.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            _categoryContext.Update(category);
+            await _categoryContext.SaveChangesAsync();
+
+            return category;
+        }
     }
 }
