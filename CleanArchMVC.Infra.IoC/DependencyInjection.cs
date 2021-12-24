ff60f577fedcbcb406e4dcc9fe5be9e810c60831
Victor Oliveira <victor.souza.41@hotmail.com>
2021-12-24 10:53:21 -0300

    O que foi alterado/adicionado:
    1-) CategoriesController.cs
     Criando controlador que por enquanto traz somente todas as categorias cadastradas no banco de dados.
    
    2-) Index.cshtml
     Criado index do controlador Categories para exibir em tela as categorias cadastradas no banco de dados.
    
    3-) startup.cs
     Modificado a classe statup para iniciar o projeto à partir da pasta "Categories/index.cshtml"

--- CleanArchMVC.Infra.IoC/DependencyInjection.cs
+++ CleanArchMVC.Infra.IoC/DependencyInjection.cs
@@ -1,27 +1,30 @@
-﻿using CleanArchMVC.Domain.Interface;
+﻿using CleanArchMVC.Application.Interfaces;
+using CleanArchMVC.Application.Mappings;
+using CleanArchMVC.Application.Services;
+using CleanArchMVC.Domain.Interface;
 using CleanArchMVC.Infra.Data.Context;
 using CleanArchMVC.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArchMVC.Infra.IoC
 {
-    public static class DependencyInjection 
+    public static class DependencyInjection
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             //Registrando o contexto (Provedor do BD)
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
-            //Registrando os serviços
+            //Registrando os repositórios
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+
+            //Registrando os serviços
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
             return services;
         }
