6c6b93fff205889f2d65c48a888831cc5af5df64
Victor Oliveira <victor.souza.41@hotmail.com>
2022-01-04 19:55:29 -0300

    O que foi alterado/adicionado:
    1-) HomeController.cs
     Criado controlador Home para direcionar a aplicação para as página "Categorias" e "Produtos".
    
    2-) Index.cshtml/Privacy.cshtml
     Definido estrutura da view.

--- CleanArchMVC.WebUI/Startup.cs
+++ CleanArchMVC.WebUI/Startup.cs
@@ -52,7 +52,7 @@ namespace CleanArchMVC.WebUI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Products}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
