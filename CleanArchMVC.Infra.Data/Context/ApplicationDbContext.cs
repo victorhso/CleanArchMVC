974fbf563d3b1efed697ad3e1c7ef920c0a0ecc1
Victor Oliveira <victor.souza.41@hotmail.com>
2022-01-08 16:37:55 -0300

    O que foi alterado/adicionado:
    Geral: Foram instalados as dependências: Microsoft.AspNetCore.Identity.EntityFrameworkCore versão 5.0.5 e Microsoft.AspNetCore.Identity versão 2.2.0
    
    1-) ApplicationDbContext.cs
     Trocado o contexto de Herança, para o contexto herdar de IdentityDbContext
    
    2-) ApplicationUser.cs
     Criado classe que herda de IdentityUser
    
    3-) -
     Gerado uma nova migração e dado update-database, para se registrar novas tabelas para uso do Identity.

--- CleanArchMVC.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArchMVC.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,19 +1,15 @@
 ﻿using CleanArchMVC.Domain.Entities;
+using CleanArchMVC.Infra.Data.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArchMVC.Infra.Data.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
 
