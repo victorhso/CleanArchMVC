5afa4e8f6298f49ed1970e926c113e5b87e67ca5
Victor Oliveira <victor.souza.41@hotmail.com>
2022-01-08 19:23:10 -0300

    O que foi alterado/adicionado:
    1-) DependecyInjection.cs
     Registrado serviços do identity.

--- CleanArchMVC.Infra.IoC/DependencyInjection.cs
+++ CleanArchMVC.Infra.IoC/DependencyInjection.cs
@@ -1,10 +1,13 @@
 ﻿using CleanArchMVC.Application.Interfaces;
 using CleanArchMVC.Application.Mappings;
 using CleanArchMVC.Application.Services;
+using CleanArchMVC.Domain.Account;
 using CleanArchMVC.Domain.Interface;
 using CleanArchMVC.Infra.Data.Context;
+using CleanArchMVC.Infra.Data.Identity;
 using CleanArchMVC.Infra.Data.Repositories;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +21,16 @@ namespace CleanArchMVC.Infra.IoC
         {
             //Registrando o contexto (Provedor do BD)
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            //Configurando Cookie do aplicativo
+            //Quando o usuário acessar um recurso que não tiver acesso, ele será redirecionado para Login.
+            services.ConfigureApplicationCookie(option => option.AccessDeniedPath = "/Account/Login");
+
+            //Registrando os serviços do Identity
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
 
             //Registrando os repositórios
             services.AddScoped<ICategoryRepository, CategoryRepository>();
