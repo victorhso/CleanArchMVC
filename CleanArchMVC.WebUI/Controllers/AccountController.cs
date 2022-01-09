6ba78a6de884d75ea5c4ea1d6ad369b2abd47a74
Victor Oliveira <victor.souza.41@hotmail.com>
2022-01-08 21:09:46 -0300

    O que foi alterado/adicionado:
    1-) AccountController.cs
     Implementado os métodos Login, Register e Logout.

--- CleanArchMVC.WebUI/Controllers/AccountController.cs
+++ CleanArchMVC.WebUI/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using CleanArchMVC.Domain.Account;
+using CleanArchMVC.WebUI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace CleanArchMVC.WebUI.Controllers
 {
@@ -9,6 +11,60 @@ namespace CleanArchMVC.WebUI.Controllers
         public AccountController(IAuthenticate authenticate)
         {
             _authenticate = authenticate;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnURL)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnURL,
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await _authenticate.Authenticate(model.DS_EMAIL, model.DS_PASSWORD);
+
+            if (result)
+            {
+                if(string.IsNullOrEmpty(model.ReturnUrl))
+                    return RedirectToAction("Index", "Home");
+                
+                return Redirect(model.ReturnUrl);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Tentativa de login inválida");
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(RegisterViewModel model)
+        {
+            var result = await _authenticate.RegisterUser(model.DS_EMAIL, model.DS_PASSWORD);
+
+            if (result)
+                return Redirect("/");
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Tentativa de registro inválida");
+                return View(model);
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _authenticate.Logout();
+            return RedirectToAction("/Account/Login");
         }
     }
 }
