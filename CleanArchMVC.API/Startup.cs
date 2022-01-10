188551c079ef38d7aa6577063fcd7becc2e5fdb0
Victor Oliveira <victor.souza.41@hotmail.com>
2022-01-10 14:43:25 -0300

    O que foi alterado/adicionado:
    1-) DependecyInjectionAPI.cs
     Criado nova classe para estar restrita somente à biblioteca API

--- CleanArchMVC.API/Startup.cs
+++ CleanArchMVC.API/Startup.cs
@@ -1,3 +1,4 @@
+using CleanArchMVC.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +27,7 @@ namespace CleanArchMVC.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddInfrastructureAPI(Configuration);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
---
+++ CleanArchMVC.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,45 @@
+﻿using CleanArchMVC.Application.Interfaces;
+using CleanArchMVC.Application.Mappings;
+using CleanArchMVC.Application.Services;
+using CleanArchMVC.Domain.Account;
+using CleanArchMVC.Domain.Interface;
+using CleanArchMVC.Infra.Data.Context;
+using CleanArchMVC.Infra.Data.Identity;
+using CleanArchMVC.Infra.Data.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CleanArchMVC.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Registrando o contexto (Provedor do BD)
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            //Registrando os serviços do Identity
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+
+            //Registrando os repositórios
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            //Registrando os serviços
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            var myHandlers = AppDomain.CurrentDomain.Load("CleanArchMVC.Application");
+            services.AddMediatR(myHandlers);
+
+            return services;
+        }
+    }
+}
