f9919507a22d73349811737c2c6ff73b31379f5b
Victor Oliveira <victor.souza.41@hotmail.com>
2022-01-10 21:06:44 -0300

    O que foi alterado/adicionado:
    1-) CategoriesController.cs
     Foi criado os endpoints (GET, PUT, POST e REMOVE) para interação através do Swagger.

---
+++ CleanArchMVC.API/Controllers/CategoriesController.cs
@@ -0,0 +1,76 @@
+﻿using CleanArchMVC.Application.DTOs;
+using CleanArchMVC.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+
+            if (categories == null)
+                return NotFound("Categorias não encontradas.");
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{id:int}", Name = "GetCategory")]
+        public async Task<ActionResult<CategoryDTO>> GetById(int id)
+        {
+            var categoryDTO = await _categoryService.GetById(id);
+
+            if (categoryDTO == null)
+                return NotFound("Categoria não encontrada.");
+
+            return Ok(categoryDTO);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDTO)
+        {
+            if (categoryDTO == null)
+                return BadRequest("Dados inválidos.");
+
+            await _categoryService.Add(categoryDTO);
+
+            return new CreatedAtRouteResult("GetCategory", new {id = categoryDTO.ID}, categoryDTO);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(int id, [FromBody] CategoryDTO categoryDTO)
+        {
+            if (id != categoryDTO.ID || categoryDTO == null)
+                return BadRequest();
+
+            await _categoryService.Update(categoryDTO);
+
+            return Ok(categoryDTO);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Delete(int id)
+        {
+            var categoryDTO = await _categoryService.GetById(id);
+
+            if (categoryDTO == null)
+                return NotFound("Categoria não encontrada.");
+
+            await _categoryService.Remove(id);
+            return Ok(categoryDTO);
+        }
+    }
+}
--- CleanArchMVC.Application/DTOs/CategoryDTO.cs
+++ CleanArchMVC.Application/DTOs/CategoryDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace CleanArchMVC.Application.DTOs
 {
