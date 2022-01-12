5a56fcde42dc7a297b8dbdf73b634a348c8862b2
Victor Oliveira <victor.souza.41@hotmail.com>
2022-01-12 11:40:13 -0300

    O que foi alterado/adicionado:
    1-) LoginModel.cs
     Criado DTO para armazenar as informações que serão utilizadas para JWT.
    
    2-) RegisterModel.cs
     Criado DTO para armazenar as informações que serão utilizadas para JWT.
    
    3-) UserToken.cs
     Criado DTO para armazenar as informações que serão utilizadas para JWT.

---
+++ CleanArchMVC.API/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMVC.API.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [EmailAddress(ErrorMessage = "Formato de e-mail inválido")]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        [StringLength(20, ErrorMessage = "O {0} deve ter pelo menos {2} e no máximo " +
+             "{1} caracteres de comprimento.", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+    }
+}
---
+++ CleanArchMVC.API/Models/RegisterModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMVC.API.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirme a senha")]
+        [Compare("Password", ErrorMessage = "As senhas não batem")]
+        public string ConfirmPassword { get; set; }
+    }
+}
